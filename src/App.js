535395e68bf83a3b00935f368d41efbf8eaf1e41
Amad-Mlinka <amadovski@gmail.com>
2021-04-06 01:40:51 +0200

    Navbar progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Navbar } from './Components/Navbar/Navbar';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <Router>
+     <Navbar/>
+     <Switch>
+       <Route path="/" exact></Route>
+     </Switch>
+    </Router>
       
-    </div>
+    </>
   );
 }
 
